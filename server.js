ce3d75376a23d9bebb19f16d9cc82248df263db8
timothysepulvado <timothysepulvado@yahoo.com>
2021-09-17 19:59:56 -0700

    New Project

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true });
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-  console.log(`listening on PORT ${PORT}`);
+app.get('/', (req, res)=>{
+    res.json({ message: "welcome "});
+})
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`listening on PORT ${PORT}`);
+  });
 });
