2c2afde8ad374e4174167d0ee6a0b427a75bf8c7
timothysepulvado <timothysepulvado@yahoo.com>
2021-09-18 12:07:55 -0700

    starting navbar

---
+++ client/src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar'
+
+function Layout(props) {
+    return (
+        <div>
+<Navbar></Navbar>
+            <p> Hello</p>
+            
+        </div>
+    );
+}
+
+export default Layout;
