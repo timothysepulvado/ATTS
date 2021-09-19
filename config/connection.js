a638342cda1235dd46e7cb830f3f5a1ca13ee2c5
timothysepulvado <timothysepulvado@yahoo.com>
2021-09-18 20:00:27 -0700

    Mongo set up

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/<project3_db_name>";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ATTS";
 
 mongoose.connect(MONGODB_URI, { })
 .then(() => console.log("you are connected to mongodb"))
